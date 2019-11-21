bbe426645935e6fdf29e07816de443e85869cc31
Gi745 <58046745+Gi745@users.noreply.github.com>
2019-11-21 10:18:05 -0800

    Create runPost.php

---
+++ runPost.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+<head>
+
+<body>
+
+
+<?php 
+
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	echo "your name is ".$name." and your email is ".$email;
+?>
+
+<body>
+<html>
