9ab78b135574705326746b7cfdf237bf90d19ca0
Gi745 <58046745+Gi745@users.noreply.github.com>
2019-11-21 10:17:40 -0800

    Create runGet.php

---
+++ runGet.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+
+<head>
+
+<body>
+
+
+<?php 
+
+	$name = $_GET["name"];
+	$email = $_GET["email"];
+	echo "your name is ".$name." and your email is ".$email;
+?>
+
+<body>
+<html>
