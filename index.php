9da0fb866131b4136509e98c331547fb2550288b
Gi745 <58046745+Gi745@users.noreply.github.com>
2019-11-21 11:30:49 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+<style>
+
+body {background-color: black;}
+h1 {
+  color: green;
+}
+#lala{
+	position:absolute;
+	right:225px;
+	top:225px;
+}
+</style>
+
+
+</head>
+
+<body>
+<h1 >This is my Index Page ;)</h1>
+<hr>
+<img id = "lala" width="300" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi_ZDh6mXUeOgk_y4XTSJt0d3ZTP0YknXBHJiXZe6TWO5d_nTu&s">
+
+
+<a href="test.php"> <h1>test.php</h1></a><br>
+<a href="form.php"> <h1>form.php</h1></a><br>
+<a href="runGet.php"> <h1>runGet.php</h1></a><br>
+<a href="runPost.php"> <h1>runPost.php</h1></a><br>
+<a href="calcForm.php"> <h1>calcForm.php</h1></a><br>
+
+
+
+
+
+</body>
+
+</html>
